e030b9612b7516ab9ea9296c1b98959d2c0eac03
Yuri Sakhno <ysakhno@gmail.com>
2024-04-03 16:00:20 -0700

    Added some very basic UI

---
+++ src/main/java/io/ysakhno/urlshortnr/controller/UIController.java
@@ -0,0 +1,13 @@
+package io.ysakhno.urlshortnr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UIController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
